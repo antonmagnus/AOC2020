08eef4049c78091b286635fe71e0d784129504cf
Anton Magnusson <magnusson27@gmail.com>
2020-12-03 09:35:35 +0100

    3\1 correct

--- 3/1/sol.py
+++ 3/1/sol.py
@@ -1,20 +1,11 @@
-f  = open("1\input.txt")
-pwList = f.readlines()
+f  = open("3\\1\\input.txt")
+treeLines = f.readlines()
 
-numOfValid = 0
-numOfInValid = 0
-for password in pwList:
-    segments = password.split(' ')
-    lowerLimit = segments[0].split('-')[0]
-    upperLimit = segments[0].split('-')[1]
-    letter = segments[1].strip(':')
-    count = segments[2].strip('\n').count(letter)
-    if count>=int(lowerLimit) and count<=int(upperLimit):
-        numOfValid += 1
-    else:
-        numOfInValid +=1
-print("invalid: " + str(numOfInValid))
-print("valid: " + str(numOfValid))
+numOfTrees = 0
+i=0
+for line in treeLines:
+    if line[i%(len(line)-1)] == '#':
+        numOfTrees +=1
+    i+=3
+print("valid: " + str(numOfTrees))
     
-
-
